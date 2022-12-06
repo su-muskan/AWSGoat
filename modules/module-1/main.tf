cb7fbcfb109a7b788480e4bc5620e30745473255
su-muskan <42991782+su-muskan@users.noreply.github.com>
2022-12-06 18:43:43 +0000

    Update main.tf

--- modules/module-1/main.tf
+++ modules/module-1/main.tf
@@ -3400,7 +3400,7 @@ resource "aws_security_group" "goat_sg" {
 
 # Instance Requirements
 resource "aws_iam_instance_profile" "goat_iam_profile" {
-  name = "AWS_GOAT_ec2_profile"
+  name = "AWS_GOAT_ec2_profile_1"
   role = aws_iam_role.goat_role.name
 }
 resource "aws_iam_role" "goat_role" {
